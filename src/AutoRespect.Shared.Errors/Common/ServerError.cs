edaef5091689da35585eaa3554d0bfc7ec180538
alex troshkin <alextroshkin@outlook.com>
2017-11-05 14:11:45 +0300

    [errors] add project with shared errors

---
+++ src/AutoRespect.Shared.Errors/Common/ServerError.cs
@@ -0,0 +1,11 @@
+﻿using AutoRespect.Infrastructure.ErrorHandling;
+
+namespace AutoRespect.Shared.Errors.Common
+{
+    public class ServerError : Error
+    {
+        public ServerError() : base("3DD50389-301C-43CD-BB08-75B9D50D2AFE", "Server error") //TODO: make better description ...
+        {
+        }
+    }
+}
